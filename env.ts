061f995c86612c9eb1402f167f9ce6b5c6166020
Mr-MiracL <lonel1nessgu@gmail.com>
2025-04-05 22:45:08 +0100

    Completed Fan out implementation - added emailing logic1

--- env.ts
+++ env.ts
@@ -1,3 +1,3 @@
 export const SES_REGION = 'eu-west-1';
-export const SES_EMAIL_FROM = '20109111@mail.wit.ie' ;
-export const SES_EMAIL_TO =  'lonel1nessgu@gmail.com';
+export const SES_EMAIL_FROM = 'email1' ;
+export const SES_EMAIL_TO =  'email2';
